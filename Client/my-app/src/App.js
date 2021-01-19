7a349bb6f2d7899793f41fdb69567fe80ac11c58
AbdullahAleem <abdullahaleem2102@gmail.com>
2021-01-20 00:33:09 +0500

    setting up server

--- Client/my-app/src/App.js
+++ Client/my-app/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';import logo from './logo.svg';import './App.css';import socketClient  from "socket.io-client";
+const SERVER = "http://127.0.0.1:8080";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  var connectionOptions = {
+    "force new connection": true,
+    "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
+    "timeout": 10000,                  //before connect_error and connect_timeout are emitted.
+    "transports": ["websocket"]
+};
+  var socket = socketClient (SERVER,connectionOptions);
+    socket.on('connection', () => {
+        console.log(`I'm connected with the back-end`);
+});
+    return (
+        <div classname="App">
 
-export default App;
+            <header classname="App-header"></header>
+
+                <img src="{logo}" classname="App-logo" alt="logo"></img>
+                <p></p>        
+                Edit <code>src/App.js</code> and save to reload.  
+               <p></p>‍
+               
+               </div>
+
+    );
+                   }
+                   
+                   export default App;
--- Servers/server.js
+++ Servers/server.js
@@ -1,15 +1,15 @@
-var app= require('express');
-// const { isObject } = require('util');
-var http=require('http').createServer(app);
-//calling socket io
-var io=require('socket.io')(http);
-//setting up the server and setting the localhost port for the communication
-const PORT= 4001;
-//setting up a socket listener for the server
-io.on('connection', ()=>{
-    console.log("client connected");
-})
-//listen what is comming to the server 
-http.listen(PORT,()=>{
-    console.log(`listening on ${PORT}`);
-})
+var app = require('express')();
+var http = require('http').createServer(app);
+const PORT = 8080;
+var io = require('socket.io')(http);
+//array of the current channels in the server
+const STATIC_CHANNELS = ['global_notifications', 'global_chat'];
+
+io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
+    console.log('new client connected');
+    socket.emit('connection', null);
+});
+//listen statement
+http.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
+});
